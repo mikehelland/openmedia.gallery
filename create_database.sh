fdec0174675030e2d9f7ad38fb392408fad5db9a
mikehelland <mike@mikehelland.com>
2020-05-05 19:58:04 -0700

    create database script

--- create_database.sh
+++ create_database.sh
@@ -17,9 +17,9 @@ fi
 echo creating database...
 
 sudo -u postgres psql postgres -c "CREATE DATABASE $OMG_DB_NAME"
-sudo -u postgres psql $OMG_DB_NAME -c "CREATE USER '$OMG_DB_NAME' WITH PASSWORD '$OMG_DB_PW'"
+sudo -u postgres psql $OMG_DB_NAME -c "CREATE USER $OMG_DB_NAME WITH PASSWORD '$OMG_DB_PW'"
 sudo -u postgres psql $OMG_DB_NAME -c "CREATE TABLE users (id bigserial primary key, created_at timestamp not null default current_timestamp, last_login timestamp, username char(20) not null, bpassword char(60) not null, btc_address char(35), admin boolean, sources jsonb, uploaded_bytes bigint default 0, upload_limit bigint default 0)"
-sudo -u postgres psql $OMG_DB_NAME -c "ALTER TABLE users OWNER TO '$OMG_DB_NAME'"
+sudo -u postgres psql $OMG_DB_NAME -c "ALTER TABLE users OWNER TO $OMG_DB_NAME"
 
 echo "export OMG_DB_NAME=$OMG_DB_NAME" > runomg.sh
 echo "export OMG_DB_PW=$OMG_DB_PW" >> runomg.sh
