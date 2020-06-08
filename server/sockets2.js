e2bba78f6ab75c1214e4e1246137e9a40afa6ca7
mikehelland <mike@mikehelland.com>
2020-06-07 21:26:37 -0700

    organize sockets2

--- server/sockets2.js
+++ server/sockets2.js
@@ -1,80 +1,30 @@
 module.exports = function (app, httpsServer) {
-    
-    var rooms = {}
-    var nextId = 1
-    var sockets = {}
-    
-    app.get('/rooms', function (req, res) {
+
+    // a route for an adminstrator to see how many are online
+    app.get('/admin/rooms', function (req, res) {
         res.send(rooms);
     })
+
+    var rooms = {}
+    var sockets = {}
+    var nextId = 1
     
     const WebSocket = require('ws')
     const wss = new WebSocket.Server({server: httpsServer})
 
     wss.on("connection", socket => {
-        var name
-        var roomName
-        var room = {users: {}}
-        var id = "" + nextId++
 
+        var id = nextId++
         sockets[id] = socket
-
-        var send = msg => {
-            socket.send(JSON.stringify(msg))
-        }
-        var sendToRoom = msg => {
-            var str = JSON.stringify(msg)
-            for (var user in room.users) {
-                if (user !== name && sockets[room.users[user].id] &&
-                        sockets[room.users[user].id].readyState === WebSocket.OPEN) {
-                    sockets[room.users[user].id].send(str)
-                }
-            }
-        }
-        var sendToId = (id, msg) => {
-            if (sockets[id] && sockets[id].readyState === WebSocket.OPEN) {
-                sockets[id].send(JSON.stringify(msg))
-            }
-        }
-    
-        var join = msg => {
-
-            if (!msg.name || !msg.room) {
-                return
-            }
-    
-            if (!rooms[msg.room]) {
-                rooms[msg.room] = {users: {}}
-            }
-            room = rooms[msg.room]
-            name = msg.name
-            roomName = msg.room
-    
-            //todo what if this user already exists
-            room.users[name] = {id: id, data: msg.data, name: name}
-    
-            sendToRoom({msgtype: "update-user-list", data: room.users})
-            send({msgtype: "joined", data: room.users})
-            if (room.data) {
-                send({msgtype:"command", command: room.data});
-
-            }
-        }
-
-        var leave = () => {
-            if (name) {
-                delete room.users[name]
-            }
-            sendToRoom({msgtype: "userLeft", name: name})
-        }
-        
+        var name
+        var room = {users:{}}
 
         socket.on("close", (code, reason) => {
             if (name) {
                 delete room.users[name]
+                sendToRoom({msgtype: "userDisconnected", name: name})
             }
             delete sockets[id]
-            sendToRoom({msgtype: "userDisconnected", name: name})
         })
 
 
@@ -95,29 +45,13 @@ module.exports = function (app, httpsServer) {
                 leave()
             }
             else if (msg.msgtype === "signaling") {
-                signaling(msg)
+                signal(msg)
             }
             else {
                 console.log(msg.msgtype)
             }
         })
-
-        var signaling = signal => {
-            signal.fromId = id
-            if (!signal.from) {
-                signal.from = name
-            }
-            try {
-                if (signal.toId) {
-                    sendToId(signal.toId, signal)
-                }
-                else if (signal.room) {
-                    sendToRoom(signal)
-                }
-            }
-            catch (e) {}
-        }
-
+        
 
         /*
         socket.on("updateLocalUserData", data => {
@@ -138,6 +72,71 @@ module.exports = function (app, httpsServer) {
 
 
 */
+
+        var send = msg => {
+            socket.send(JSON.stringify(msg))
+        }
+        var sendToRoom = msg => {
+            var str = JSON.stringify(msg)
+            for (var user in room.users) {
+                if (user !== name && sockets[room.users[user].id] &&
+                        sockets[room.users[user].id].readyState === WebSocket.OPEN) {
+                    sockets[room.users[user].id].send(str)
+                }
+            }
+        }
+        var sendToId = (id, msg) => {
+            if (sockets[id] && sockets[id].readyState === WebSocket.OPEN) {
+                sockets[id].send(JSON.stringify(msg))
+            }
+        }
+
+        var join = msg => {
+
+            if (!msg.name || !msg.room) {
+                return
+            }
+
+            if (!rooms[msg.room]) {
+                rooms[msg.room] = {users: {}}
+            }
+            room = rooms[msg.room]
+            name = msg.name
+            roomName = msg.room
+
+            //todo what if this user already exists
+            room.users[name] = {id, name, data: msg.data}
+
+            sendToRoom({msgtype: "update-user-list", data: room.users})
+            send({msgtype: "joined", data: room.users})
+            if (room.data) {
+                send({msgtype:"command", command: room.data});
+
+            }
+        }
+
+        var leave = () => {
+            if (name) {
+                delete room.users[name]
+            }
+            sendToRoom({msgtype: "userLeft", name})
+        }
+
+        var signal = signal => {
+            signal.fromId = id
+            if (!signal.from) {
+                signal.from = name
+            }
+            try {
+                if (signal.toId) {
+                    sendToId(signal.toId, signal)
+                }
+                else if (signal.room) {
+                    sendToRoom(signal)
+                }
+            }
+            catch (e) {}
+        }
 
     })
 
